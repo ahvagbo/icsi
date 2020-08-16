290b9667441cbf6dc1adea57bf350abc284ce9ee
Iurie <iuriejurja@hotmail.com>
2020-08-16 13:15:35 +0300

    Updated Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
+using ICsi.Core;
 using ICsi.Roslyn;
 
 using static System.Console;
@@ -12,66 +13,69 @@ namespace ICsi
 {
     internal static class Program
     {
-        private static void ReportDiagnostics(ImmutableArray<Diagnostic> diagnostics)
-        {
-            foreach (Diagnostic diagnostic in diagnostics)
-            {
-                if (diagnostic.Severity == DiagnosticSeverity.Error)
-                    ForegroundColor = ConsoleColor.DarkRed;
-                else if (diagnostic.Severity == DiagnosticSeverity.Warning)
-                    ForegroundColor = ConsoleColor.Yellow;
-                else
-                    ForegroundColor = ConsoleColor.DarkGray;
+        //private static void ReportDiagnostics(ImmutableArray<Diagnostic> diagnostics)
+        //{
+        //    foreach (Diagnostic diagnostic in diagnostics)
+        //    {
+        //        if (diagnostic.Severity == DiagnosticSeverity.Error)
+        //            ForegroundColor = ConsoleColor.DarkRed;
+        //        else if (diagnostic.Severity == DiagnosticSeverity.Warning)
+        //            ForegroundColor = ConsoleColor.Yellow;
+        //        else
+        //            ForegroundColor = ConsoleColor.DarkGray;
             
-                WriteLine(diagnostic);
-                ResetColor();
-            }
-        }
+        //        WriteLine(diagnostic);
+        //        ResetColor();
+        //    }
+        //}
 
-        private static void Main(string[] args)
-        {
-            var engineOptions = ScriptEngineOptions.Default;
-            var engine = new SimpleScriptEngine(engineOptions);
-            var parseOptions = CSharpParseOptions.Default
-                                                 .WithLanguageVersion(engineOptions.Version)
-                                                 .WithKind(SourceCodeKind.Script);
-            
-            bool IsComplete(string submission)
-            {
-                var tree = CSharpSyntaxTree.ParseText(submission,
-                                                      parseOptions);
-                return SyntaxFactory.IsCompleteSubmission(tree);
-            }
+        //private static void Main(string[] args)
+        //{
+        //    var engineOptions = ScriptEngineOptions.Default;
+        //    var engine = new SimpleScriptEngine(engineOptions);
+        //    var parseOptions = CSharpParseOptions.Default
+        //                                         .WithLanguageVersion(engineOptions.Version)
+        //                                         .WithKind(SourceCodeKind.Script);
 
-            while (true)
-            {
-                Write("> ");
-                string code = ReadLine();
+        //    bool IsComplete(string submission)
+        //    {
+        //        var tree = CSharpSyntaxTree.ParseText(submission,
+        //                                              parseOptions);
+        //        return SyntaxFactory.IsCompleteSubmission(tree);
+        //    }
 
-                while (!IsComplete(code))
-                {
-                    Write("* ");
-                    string line = ReadLine();
-                    code += line;
-                }
+        //    while (true)
+        //    {
+        //        Write("> ");
+        //        string code = ReadLine();
 
-                try
-                {
-                    ScriptResult result = engine.Run(code);
+        //        while (!IsComplete(code))
+        //        {
+        //            Write("* ");
+        //            string line = ReadLine();
+        //            code += line;
+        //        }
 
-                    if (result.CompilationException != null)
-                        ReportDiagnostics(result.CompilationException.Diagnostics);
-                    else if (result.ExecutionException != null)
-                        WriteLine($"{result.ExecutionException.GetType()} = {result.ExecutionException.Message}");
-                    else
-                        WriteLine(result.ReturnValue);
-                }
+        //        try
+        //        {
+        //            ScriptResult result = engine.Run(code);
 
-                catch (Exception ex)
-                {
-                    WriteLine($"{ex.GetType()}: {ex.Message}");
-                }
-            }
-        }
+        //            if (result.CompilationException != null)
+        //                ReportDiagnostics(result.CompilationException.Diagnostics);
+        //            else if (result.ExecutionException != null)
+        //                WriteLine($"{result.ExecutionException.GetType()} = {result.ExecutionException.Message}");
+        //            else
+        //                WriteLine(result.ReturnValue);
+        //        }
+
+        //        catch (Exception ex)
+        //        {
+        //            WriteLine($"{ex.GetType()}: {ex.Message}");
+        //        }
+        //    }
+        //}
+
+        private static void Main()
+            => new Repl().Run();
     }
 }
