1b9ebdd9ff5759f4f4fd71f0c5749d388998df0f
Iurie <iuriejurja@hotmail.com>
2020-08-24 20:18:40 +0300

    Updated ScriptEngineOptions.cs

--- src/Roslyn/ScriptEngineOptions.cs
+++ src/Roslyn/ScriptEngineOptions.cs
@@ -14,14 +14,14 @@ namespace ICsi.Roslyn
         public LanguageVersion Version { get; }
         public int WarningLevel { get; }
         public bool AllowUnsafe { get; }
-        public string[] Imports { get; }
-        public string[] References { get; }
+        public List<string> Imports { get; }
+        public List<string> References { get; }
 
         internal ScriptEngineOptions(LanguageVersion version,
                                      int warningLevel,
                                      bool allowUnsafe,
-                                     string[] imports,
-                                     string[] references)
+                                     List<string> imports,
+                                     List<string> references)
         {
             Version = version;
             WarningLevel = warningLevel;
@@ -34,15 +34,16 @@ namespace ICsi.Roslyn
             => new ScriptEngineOptions(version: LanguageVersion.CSharp9,
                                        warningLevel: 1,
                                        allowUnsafe: false,
-                                       new string[] {},
-                                       new string[]
+                                       new List<string> {},
+                                       new List<string>
                                        {
                                            typeof(object).Assembly.Location,
                                            typeof(Console).Assembly.Location,
                                            typeof(File).Assembly.Location,
                                            typeof(IEnumerable).Assembly.Location,
                                            typeof(IEnumerable<>).Assembly.Location,
-                                           typeof(ImmutableArray<>).Assembly.Location
+                                           typeof(ImmutableArray<>).Assembly.Location,
+                                           typeof(Enumerable).Assembly.Location
                                        });
 
         public ScriptEngineOptions WithVersion(LanguageVersion version)
@@ -67,9 +68,9 @@ namespace ICsi.Roslyn
                                        References);
         
         public void AddImports(string[] imports)
-            => Imports.ToList().AddRange(imports);
+            => Imports.AddRange(imports);
 
         public void AddReferences(string[] references)
-            => References.ToList().AddRange(references);
+            => References.AddRange(references);
     }
 }
