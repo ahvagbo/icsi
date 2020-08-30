556ff36018be1f0256b334b2eb519055692298f3
Iurie <iuriejurja@hotmail.com>
2020-08-30 21:37:59 +0300

    Updated Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,10 @@
 ﻿using ICsi.Core;
 
-using McMaster.Extensions.CommandLineUtils;
-
 namespace ICsi
 {
     internal static class Program
     {
         private static int Main(string[] args)
-            => CommandLineApplication.Execute<Application>(args);
+            => new Application().Run(args);
     }
 }
